6458f037f46e4a01c2c8c67d9fe1dc7ffaba3dc4
TonyDon <tonydon140@qq.com>
2022-11-29 11:02:32 +0800

    update https

--- src/main/java/top/tonydon/dns/constant/ClientConstants.java
+++ src/main/java/top/tonydon/dns/constant/ClientConstants.java
@@ -2,11 +2,11 @@ package top.tonydon.dns.constant;
 
 public class ClientConstants {
 
-    public static final String DNS_API = "http://tonydon.top:6515/admin/dns/list";
+    public static final String DNS_API = "https://tonydon.top:6515/admin/dns/list";
 
-    public static final String ABOUT_URL = "http://www.tonydon.top/article/9";
+    public static final String ABOUT_URL = "https://www.tonydon.top/article/9";
 
-    public static final String LATEST_VERSION_API = "http://tonydon.top:6515/admin/project/version/latest/2";
+    public static final String LATEST_VERSION_API = "https://tonydon.top:6515/admin/project/version/latest/2";
 
     public static final String VERSION = "v0.0.2";
 
