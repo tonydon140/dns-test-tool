e49b733961954a255c7b13560c2e0abc8764150f
TonyDon <tonydon140@qq.com>
2022-12-23 16:52:10 +0800

    v0.2.0

--- python/main.py
+++ python/main.py
@@ -1,46 +1,22 @@
-import json
 import sys
 
-import requests
-from PySide6.QtCore import QObject, QThreadPool, Signal, QRunnable
+from PySide6.QtCore import QThreadPool, QUrl
+from PySide6.QtGui import QAction, QDesktopServices
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, \
-    QHeaderView
-from ping3 import ping
+    QHeaderView, QHBoxLayout, QMainWindow, QMessageBox
+
+from runnable import MySignal, CheckUpdateRunnable, TestDelayRunnable, GetDnsListRunnable
 
 
-class TestDelayRunnable(QRunnable):
-    def __init__(self, index, isPrimary, ip, signal):
-        super().__init__()
-        self.index = index
-        self.ip = ip
-        self.isPrimary = isPrimary
-        self.signal = signal
-
-    def run(self) -> None:
-        delay = ping(self.ip)
-        self.signal.emit(self.index, self.isPrimary, delay)
-
-
-class GetDnsListRunnable(QRunnable):
-    def __init__(self, signal):
-        super().__init__()
-        self.signal = signal
-
-    def run(self) -> None:
-        res = requests.get('https://tonydon.top:6515/admin/dns/list')
-        res = json.loads(res.text)
-        self.signal.emit(res['data'])
-
-
-class MySignal(QObject):
-    dns_item = Signal(int, bool, float)
-    dns_list = Signal(list)
-
-
-class StarMovie(QWidget):
+class StarMovie(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.resize(640, 520)
+        self.setWindowTitle('DNS延迟测试工具 v0.2.0')
+        self._create_menu_bar()
+
         # 成员变量
+        self.VERSION_NUMBER = 4
         self.count = 0
         self.dns_list = []
         self.pool = QThreadPool.globalInstance()
@@ -49,23 +25,42 @@ class StarMovie(QWidget):
         self.signal = MySignal()
         self.signal.dns_item.connect(self.ui_delay)
         self.signal.dns_list.connect(self.ui_dns_list)
+        self.signal.check_update.connect(self.ui_update)
 
         # 布局设置
-        self.setWindowTitle('DNS延迟测试工具')
-        self.resize(640, 520)
-        self.vbl = QVBoxLayout(self)
-        self.btn_test_delay = QPushButton('测试延迟', self)
-        self.btn_test_delay.clicked.connect(self.start_test_delay)
-        self.vbl.addWidget(self.btn_test_delay)
+        self.central_widget = QWidget()
+        self.VL_0 = QVBoxLayout(self)
+        self.HL_1 = QHBoxLayout(self)
+        self.PB_flush_dns_list = QPushButton('刷新DNS列表', self)
+        self.PB_test_delay = QPushButton('测试延迟', self)
+        self.HL_1.addWidget(self.PB_flush_dns_list)
+        self.HL_1.addWidget(self.PB_test_delay)
+        self.VL_0.addLayout(self.HL_1)
         self.table = QTableWidget(self)
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.table.setSortingEnabled(True)
-        self.vbl.addWidget(self.table)
+        self.VL_0.addWidget(self.table)
+        self.central_widget.setLayout(self.VL_0)
+        self.setCentralWidget(self.central_widget)
 
-        # 初始化逻辑
-        self.get_dns_data()
+        # 布置信号
+        self.PB_flush_dns_list.clicked.connect(self.flush_dns_list)
+        self.PB_test_delay.clicked.connect(self.start_test_delay)
 
-    def get_dns_data(self):
+        self.flush_dns_list()
+
+    def _create_menu_bar(self):
+        menu = self.menuBar().addMenu('帮助')
+        self.Act_update = QAction('检查更新', self)
+        self.Act_update.triggered.connect(self.check_update)
+        menu.addAction(self.Act_update)
+
+    def check_update(self):
+        task = CheckUpdateRunnable(self.signal.check_update)
+        self.pool.start(task)
+
+    def flush_dns_list(self):
+        self.table.clearContents()
         runnable = GetDnsListRunnable(self.signal.dns_list)
         self.pool.start(runnable)
 
@@ -79,31 +74,49 @@ class StarMovie(QWidget):
         self.count = 0
         for i in range(len(self.dns_list)):
             dns = self.dns_list[i]
-            task = TestDelayRunnable(i, True, dns['primaryIp'], self.signal.dns_item)
-            self.pool.start(task)
-            task = TestDelayRunnable(i, False, dns['assistantIp'], self.signal.dns_item)
+            task = TestDelayRunnable(i, dns['ip'], self.signal.dns_item)
             self.pool.start(task)
 
     def ui_dns_list(self, dns_list):
         self.dns_list = dns_list
-        self.table.setColumnCount(5)
+        self.table.setColumnCount(4)
         self.table.setRowCount(len(self.dns_list))
-        self.table.setHorizontalHeaderLabels(["主IP", "副IP", "信息", '主IP延迟', '副IP延迟'])
+        self.table.setHorizontalHeaderLabels(["描述", "DNS地址", '延迟（ms）', "信息"])
         for i in range(len(self.dns_list)):
             dns = self.dns_list[i]
-            self.table.setItem(i, 0, QTableWidgetItem(dns['primaryIp']))
-            self.table.setItem(i, 1, QTableWidgetItem(dns['assistantIp']))
-            self.table.setItem(i, 2, QTableWidgetItem(dns['description']))
+            self.table.setItem(i, 0, QTableWidgetItem(dns['description']))
+            self.table.setItem(i, 1, QTableWidgetItem(dns['ip']))
 
-    def ui_delay(self, index, isPrimary, delay):
+    def ui_delay(self, index, success, fail, delay):
         self.count += 1
 
-        delay = 'Timeout' if delay == 0 else f'{round(delay * 1000)}ms'
-        col = 3 if isPrimary else 4
-        self.table.setItem(index, col, QTableWidgetItem(delay))
+        delay = 'Timeout' if delay == 0 else f'{round(delay * 1000)}'
 
-        if self.count == len(self.dns_list) * 2:
+        self.table.setItem(index, 2, QTableWidgetItem(delay))
+        self.table.setItem(index, 3, QTableWidgetItem(f'成功：{success}次，失败：{fail}次'))
+
+        if self.count == len(self.dns_list):
             self.table.setSortingEnabled(True)
+
+    def ui_update(self, res):
+        if res['code'] != 200:
+            QMessageBox.information(self, "检查更新", "网络错误！请检查网络或联系作者。")
+            return
+
+        # 解析数据，检查更新
+        data = res['data']
+        if data['versionNumber'] <= self.VERSION_NUMBER:
+            QMessageBox.information(self, "检查更新", "当前版本已是最新版本！")
+            return
+
+        # 有新版本
+        text = f"有可用的最新版本！{data['version']}\n{data['description']}\n是否前往官网下载更新？"
+        btn = QMessageBox.information(self, "检查更新", text, QMessageBox.StandardButton.Ok,
+                                      QMessageBox.StandardButton.No)
+
+        # 打开浏览器
+        if btn is QMessageBox.StandardButton.Ok:
+            QDesktopServices.openUrl(QUrl("https://gitee.com/shuilanjiao/dns-test-tool/releases"))
 
 
 if __name__ == '__main__':
