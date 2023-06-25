d6d1f5eb5e2d4f7d25764af51874485a3b764e77
TonyDon <tonydon140@qq.com>
2023-06-25 09:46:39 +0800

    修改版本号；测试build.yml

--- src/main/kotlin/top/tonydon/tools/dns/constant/ClientConstants.kt
+++ src/main/kotlin/top/tonydon/tools/dns/constant/ClientConstants.kt
@@ -4,7 +4,7 @@ object ClientConstants {
     const val DNS_API = "https://tonydon.top:6515/admin/dns/list"
     const val ABOUT_URL = "https://www.tonydon.top/article/9"
     const val LATEST_VERSION_API = "https://tonydon.top:6515/admin/project/version/latest/2"
-    const val VERSION = "v0.3.0"
+    const val VERSION = "v1.0.0"
     const val TITLE = "DNS测试工具 $VERSION"
     const val VERSION_NUMBER = 5
     const val LATEST_URL = "https://github.com/tonydon140/dns-test-tool/releases"
